d3ae7d35705d6dd07504c33ab06b5816858afc46
Sergy <ascirex@gmail.com>
2017-09-11 01:20:55 +0300

    Base view model added

---
+++ Clinic/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Clinic.ViewModels.Base
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class MagicAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class NoMagicAttribute : Attribute
+    {
+    }
+
+    [Magic]
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)] // to preserve method call 
+        protected static void Raise()
+        {
+        }
+    }
+}
---
+++ Clinic/ViewModels/Command.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Clinic.ViewModels.Base
+{
+    public class Command : ICommand
+    {
+        private Predicate<object> _canExecute;
+        private Action<object> _execute;
+
+        public Command(Action<object> execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public Command(Action execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public Command(Action execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            if (canExecute == null)
+                throw new ArgumentNullException("canExecute");
+
+            _execute = obj => execute.Invoke();
+            _canExecute = canExecute;
+        }
+
+        public Command(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            if (canExecute == null)
+                throw new ArgumentNullException("canExecute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (_canExecute != null) && _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+
+        public static ICommand New(Action execute)
+        {
+            return new Command(execute);
+        }
+
+        public static ICommand New(Action<object> execute)
+        {
+            return new Command(execute);
+        }
+
+        private event EventHandler CanExecuteChangedInternal;
+
+        public void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChangedInternal;
+            //DispatcherHelper.BeginInvokeOnUIThread(() => handler.Invoke(this, EventArgs.Empty));
+            handler?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Destroy()
+        {
+            _canExecute = _ => false;
+            _execute = _ => { };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
