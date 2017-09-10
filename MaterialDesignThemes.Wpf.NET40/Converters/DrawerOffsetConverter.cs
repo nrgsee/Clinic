284a4164ef2e4268c8cebfdf5f657c6b8bcc9149
Daniil <daniil.rozputnii@gmail.com>
2017-09-11 00:44:20 +0300

    Init project

---
+++ MaterialDesignThemes.Wpf.NET40/Converters/DrawerOffsetConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{    
+    public class DrawerOffsetConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = value as double? ?? 0;
+            if (double.IsInfinity(d) || double.IsNaN(d)) d = 0;
+            return 0 - d;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MaterialDesignThemes.Wpf.NET40/TreeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MaterialDesignThemes.Wpf
+{
+    internal static class TreeHelper
+    {
+        public static Visual FindMainTreeVisual(Visual visual)
+        {
+            DependencyObject root = null;
+            DependencyObject dependencyObject = visual;
+
+            while (dependencyObject != null)
+            {
+                root = dependencyObject;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            return root as Visual;
+        }
+    }
+}
